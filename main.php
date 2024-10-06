7874e4e0f6408348c9593f92f107e23293fa9eb2
Yongbin Kim <iam@yongbin.kim>
2024-10-06 17:55:28 +0900

    feat: initial design?

--- main.php
+++ main.php
@@ -11,12 +11,11 @@ if (!defined('DOKU_INC')) die();
 $showSidebar = page_findnearest($conf['sidebar']);
 
 ?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang'] ?>"
-  lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>" class="no-js">
+<html lang="<?php echo $conf['lang'] ?>">
+
 <head>
     <meta charset="UTF-8" />
     <title><?php tpl_pagetitle() ?> [<?php echo strip_tags($conf['title']) ?>]</title>
-    <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
     <?php tpl_metaheaders() ?>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
@@ -24,73 +23,52 @@ $showSidebar = page_findnearest($conf['sidebar']);
 </head>
 
 <body id="dokuwiki__top">
-    <div id="dokuwiki__site" class="<?php echo tpl_classes(); ?> <?php echo ($showSidebar) ? 'hasSidebar' : ''; ?>">
+    <div class="<?php echo tpl_classes(); ?> <?php echo ($showSidebar) ? 'has-sidebar' : ''; ?>">
         <?php html_msgarea() ?>
 
-        <!-- ********** HEADER ********** -->
-        <header id="dokuwiki__header"><div class="group">
-            <?php tpl_includeFile('header.html') ?>
+        <header>
+            <div class="user-header">
+                <?php tpl_includeFile('header.html') ?>
+            </div>
 
-            <h1><?php tpl_link(wl(),$conf['title'],'accesskey="h" title="[H]"') ?></h1>
-            <?php if ($conf['tagline']): ?>
-                <p class="claim"><?php echo $conf['tagline'] ?></p>
-            <?php endif ?>
-
-            <p class="a11y skip">
-                <a href="#dokuwiki__content"><?php echo $lang['skip_to_content'] ?></a>
-            </p>
-
-            <!-- BREADCRUMBS -->
-            <?php if($conf['breadcrumbs']){ ?>
-                <div class="breadcrumbs"><?php tpl_breadcrumbs() ?></div>
-            <?php } ?>
-            <?php if($conf['youarehere']){ ?>
-                <div class="breadcrumbs"><?php tpl_youarehere() ?></div>
-            <?php } ?>
-
-            <hr />
-        </div></header><!-- /header -->
-
-
-        <div class="wrapper group">
-
-            <!-- ********** ASIDE ********** -->
-            <?php if ($showSidebar): ?>
-                <nav id="dokuwiki__aside" aria-label="<?php echo $lang['sidebar'] ?>"><div class="aside include group">
-                    <?php tpl_includeFile('sidebarheader.html') ?>
-                    <?php tpl_include_page($conf['sidebar'], 1, 1) ?>
-                    <?php tpl_includeFile('sidebarfooter.html') ?>
-                    <hr class="a11y" />
-                </div></nav><!-- /aside -->
-            <?php endif; ?>
-
-            <!-- ********** CONTENT ********** -->
-            <main id="dokuwiki__content"><div class="group">
-                <?php tpl_flush() ?>
-                <?php tpl_includeFile('pageheader.html') ?>
-
-                <div class="page group">
-                    <!-- wikipage start -->
-                    <?php tpl_content() ?>
-                    <!-- wikipage stop -->
+            <div class="main-header">
+                <div class="site-title">
+                    <h1><?php tpl_link(wl(), $conf['title']) ?></h1>
+                    <?php if ($conf['tagline']): ?>
+                        <p class="tagline"><?php echo $conf['tagline'] ?></p>
+                    <?php endif ?>
                 </div>
 
-                <?php tpl_flush() ?>
-                <?php tpl_includeFile('pagefooter.html') ?>
-            </div></main><!-- /content -->
+                <p class="a11y skip">
+                    <a href="#dokuwiki__content"><?php echo $lang['skip_to_content'] ?></a>
+                </p>
+            </div>
+        </header>
 
-        </div><!-- /wrapper -->
+        <hr />
 
-        <!-- ********** FOOTER ********** -->
+        <main>
+            <?php tpl_flush() ?>
+            <?php tpl_includeFile('pageheader.html') ?>
+
+            <div class="page group">
+                <?php tpl_content() ?>
+            </div>
+
+            <?php tpl_flush() ?>
+            <?php tpl_includeFile('pagefooter.html') ?>
+        </main>
+
+        <hr />
+
         <footer id="dokuwiki__footer">
-
-            <hr />
             <div class="doc"><?php tpl_pageinfo() ?></div>
+
             <?php tpl_license('button') ?>
 
             <nav class="tools" aria-label="<?php echo $lang['tools'] ?>">
                 <!-- SITE TOOLS -->
-                <div id="dokuwiki__sitetools">
+                <div id="dokuwiki__sitetools" class="tool-list">
                     <h3><?php echo $lang['site_tools'] ?></h3>
                     <?php tpl_searchform() ?>
                     <?php // echo (new \dokuwiki\Menu\MobileMenu())->getDropdown($lang['tools']); ?>
@@ -100,7 +78,7 @@ $showSidebar = page_findnearest($conf['sidebar']);
                 </div>
 
                 <!-- PAGE TOOLS -->
-                <div id="dokuwiki__pagetools">
+                <div id="dokuwiki__pagetools" class="tool-list">
                     <h3><?php echo $lang['page_tools'] ?></h3>
                     <ul>
                         <?php echo (new \dokuwiki\Menu\PageMenu())->getListItems('action ', false); ?>
@@ -109,16 +87,9 @@ $showSidebar = page_findnearest($conf['sidebar']);
 
                 <!-- USER TOOLS -->
                 <?php if ($conf['useacl']): ?>
-                    <div id="dokuwiki__usertools">
+                    <div id="dokuwiki__usertools" class="tool-list">
                         <h3><?php echo $lang['user_tools'] ?></h3>
                         <ul>
-                            <?php
-                                if (!empty($_SERVER['REMOTE_USER'])) {
-                                    echo '<li class="user">';
-                                    tpl_userinfo();
-                                    echo '</li>';
-                                }
-                            ?>
                             <?php echo (new \dokuwiki\Menu\UserMenu())->getListItems('action ', false); ?>
                         </ul>
                     </div>
